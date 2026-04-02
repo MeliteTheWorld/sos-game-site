6e4e50b577948d97e49d132096bf38007792da2a
Melite <pasha.nobok2025@Yandex.ru>
2026-04-02 03:25:54 +0300

    new html/css/js

--- scripts.js
+++ scripts.js
@@ -1,11 +1,71 @@
+// логика dropdown
+
 const lang = document.querySelector('.header__lang');
 const btn = document.querySelector('.lang__btn');
 
-btn.addEventListener('click', (e) => {
+btn.addEventListener('click', function(e) {
+
     e.stopPropagation();
-    lang.classList.toggle('is-open');
+
+    if (lang.classList.contains('is-open')) {
+        lang.classList.remove('is-open');
+    } else {
+        lang.classList.add('is-open');
+    }
+
 });
 
-document.addEventListener('click', () => {
+document.addEventListener('click', function() {
+
     lang.classList.remove('is-open');
+
+});
+
+// логика slider
+
+const imgs = document.querySelectorAll('.slider__img');
+const dots = document.querySelectorAll('.slider__dot');
+
+let current = 0;
+
+function updateSlider() {
+
+    for (let i = 0; i < imgs.length; i++) {
+
+        imgs[i].className = "slider__img";
+
+        if (i === current) {
+            imgs[i].classList.add("slider__img--active");
+        } else if (i === current + 1) {
+            imgs[i].classList.add("slider__img--mid");
+        } else {
+            imgs[i].classList.add("slider__img--back");
+        }
+    }
+
+    for (let i = 0; i < dots.length; i++) {
+
+        if (i === current) {
+            dots[i].classList.add("slider__dot--active");
+        } else {
+            dots[i].classList.remove("slider__dot--active");
+        }
+
+    }
+}
+
+updateSlider();
+
+const nextBtn = document.getElementById("sliderNext");
+
+nextBtn.addEventListener("click", function() {
+
+    current = current + 1;
+
+    if (current >= imgs.length) {
+        current = 0;
+    }
+
+    updateSlider();
+
 });
